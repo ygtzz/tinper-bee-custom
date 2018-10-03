5acba11ec56229606d251cc13e97738246e87e9b
ygtzz <ygtzz@126.com>
2018-10-03 15:53:11 +0800

    add tinper file build and download

--- src/controller/tinper.js
+++ src/controller/tinper.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
 const os = require('os');
 const path = require('path');
+const shell = require('shelljs');
+const send = require('koa-send');
+const archiver = require('archiver');
 require('ignore');
 require('wcwidth');
 const git = require('isomorphic-git');
@@ -20,16 +23,67 @@ module.exports = {
         await ctx.render('custom',{coms:coms});
     },
     async download(ctx){
-        console.log(ctx.query.coms);
+        const coms = ctx.query.coms || '';
+        const aComs = coms.split(',');
+        //生成index.js
+        //生成require字符串
+        var requires = [],
+            TinperBee = ["version: '1.5.0'"];
+        //生成TinperBee对象
+        aComs.forEach(function(item){
+            requires.push('var ' + item + ' = require("./lib/' + item + '");');
+            TinperBee.push(item + ':' + item);
+        });
+        requires = requires.join('');
+        TinperBee = TinperBee.join(',');
 
-        // await git.clone({
-        //     dir: './downloads',
-        //     url: 'https://github.com/iuap-design/tinper-bee.git',
-        //     singleBranch: true,
-        //     depath: 1 
-        // })
+        let tpl = `${requires}
 
-        await ctx.render('download');
+        var TinperBee = {
+            ${TinperBee}
+        }
+        
+        module.exports = TinperBee;`
+
+        let base = 'downloads/1.5.2/tinper-bee-release';
+
+        await fs.writeFileSync(base + '/index.js',tpl);
+
+        shell.cd('downloads/1.5.2/tinper-bee-release');
+        // shell.ls('*.js').forEach(function(file){
+        //     console.log(file);
+        // });
+        if (shell.exec('npm run build').code !== 0) {
+            shell.echo('Error: tinperbee build error');
+        }
+        console.log('build success');
+
+        // const filePath = '/build/tinper-bee.min.js';
+        // ctx.attachment(filePath);
+        // await send(ctx,filePath);
+
+        const zipName = 'tinper.zip';
+        const zipStream = fs.createWriteStream(zipName);
+        const zip = archiver('zip');
+        zip.pipe(zipStream);
+        base = path.join(__dirname,'../../') + base;
+        // 打包文件方式
+        const list = [
+            {path: base + '/build/tinper-bee.js',name: 'tinper-bee.js'},
+            {path: base + '/build/tinper-bee.min.js',name:'tinper-bee-min.js'},
+            {path: base + '/assets/tinper-bee.css',name:'tinper-bee.css'}
+        ];
+        for (let i = 0; i < list.length; i++) {
+            zip.append(fs.createReadStream(list[i].path), { name: list[i].name })
+        }
+        // 打包文件夹方式，有文件冗余index.css
+        // zip.directory('build/');
+        // zip.directory('assets/');
+        await zip.finalize();
+        ctx.attachment(zipName);
+        await send(ctx, zipName);
+
+        // await ctx.render('download');
     }
 }
 
---
+++ src/tpl/tinper-index.js
@@ -0,0 +1,14 @@
+`{$requires}
+
+var TinperBee = {
+    {$TinperBee}
+}
+
+module.exports = TinperBee;`
+
+// await git.clone({
+//     dir: './downloads',
+//     url: 'https://github.com/iuap-design/tinper-bee.git',
+//     singleBranch: true,
+//     depath: 1 
+// })
